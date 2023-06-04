1968423af6049edc27a597e566d4ca52ac1abeca
Ricardo Kondo <rimkondo@gmail.com>
2023-06-04 14:53:46 -0400

    Configurando firebase

---
+++ firebase_app/src/App.js
@@ -0,0 +1,11 @@
+import { db } from "./firebaseConnection";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React project + Firebase</h1>
+    </div>
+  );
+}
+
+export default App;
